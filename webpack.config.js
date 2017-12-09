9619497f9762bebc0c09951d36fba75b72f7c79a
Jaryd Krishnan <jaryd@harvestprofit.com>
2017-12-09 14:02:02 -0600

    Update webpack to product a smaller, more optimized bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,42 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
+/**
+ * This is our webpack config for the final built product.
+ * It pumps out a dist/index.js file that is ready for web-based use.
+ */
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  /* We don't need `fs`, so we pretend it doesn't exist */
   node: {
     fs: 'empty',
   },
+  /* We use uglifyjs to compile/minify our gargantuan bundle from this project */
   plugins: [
     new UglifyJSPlugin({
       parallel: true,
     }),
   ],
+  /* We use babel-loader to replicate the .babelrc file in webpack */
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "airbnb",
+              "stage-2",
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
