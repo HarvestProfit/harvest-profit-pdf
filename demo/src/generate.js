de4e83f0735f85678d994e3dd8a61cb030bb1145
Jaryd Krishnan <jaryd@harvestprofit.com>
2017-12-11 12:38:10 -0600

    Add super-simple demo

---
+++ demo/src/generate.js
@@ -0,0 +1,29 @@
+import {
+  HarvestProfitPDFHeader,
+  HarvestProfitPDFFooter,
+  PDFBuilder,
+} from 'harvest-profit-pdf';
+import saveAs from 'save-as';
+
+export default function generate() {
+  const builder = new PDFBuilder({
+    header: new HarvestProfitPDFHeader(2017),
+    footer: new HarvestProfitPDFFooter({
+      message: 'Something here',
+    }),
+    margins: {
+      bottom: -30,
+      left: 30,
+      right: 30,
+      top: 30,
+    },
+    subHeadingFontColor: '#555555',
+    subHeadingFontSize: 10,
+    includePageNumber: true,
+    title: 'Harvest Profit Is Awesome',
+  });
+
+  builder.generateBlob().then((blob) => {
+    saveAs(blob, 'Test.pdf');
+  });
+}
