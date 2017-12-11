0e64f0a512b78bbd7ad510bfab1975cb1449a5ef
Jaryd Krishnan <jaryd@harvestprofit.com>
2017-12-11 10:11:16 -0600

    Include basic PDF header and footer in main export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import BasicPDFFooter from './BasicPDFFooter';
+import BasicPDFHeader from './BasicPDFHeader';
 import HarvestProfitPDFFooter from './HarvestProfitPDFFooter';
 import HarvestProfitPDFHeader from './HarvestProfitPDFHeader';
 import PDFBuilder from './PDFBuilder';
 import PDFTable from './PDFTable';
 
 export default {
+  BasicPDFFooter,
+  BasicPDFHeader,
   HarvestProfitPDFFooter,
   HarvestProfitPDFHeader,
   PDFBuilder,
